9e79eeff98fbb306b0eee8f3736bfbec4d2b0fe6
Marc Gomez <marcgomez@microsoft.com>
2023-12-02 21:12:03 +0100

    Day 1 Part 2

--- day1/main.py
+++ day1/main.py
@@ -1,15 +1,34 @@
 import re
 
 
-def find_first_digit(line) -> int:
-    match = re.search(r'(\d)', line)
-    return int(match.group(0)) if match else None
+word_to_num = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+
+def convert(part):
+    return word_to_num.get(part) if part in word_to_num else int(part)
+
+
+def calculate(line) -> int:
+    matches = re.findall(r'(\d|one|two|three|four|five|six|seven|eight|nine)', line)
+    first = convert(matches[0])
+    last = convert(matches[-1])
+    return first * 10 + last
 
 
 with open('day1/input.txt', 'r') as file:
     content = file.read()
 
-total = sum([find_first_digit(line) * 10 + find_first_digit(line[::-1])
-             for line in content.splitlines()])
+results = [calculate(line) for line in content.splitlines()]
+total = sum(results)
 
 print(total)
