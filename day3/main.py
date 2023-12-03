cef2378ef7cfa26a1fe90ac45fc351be9ae6217a
Marc Gomez <marcgomez@microsoft.com>
2023-12-03 15:08:37 +0100

    Reuse same method for both solutions

--- day3/main.py
+++ day3/main.py
@@ -1,57 +1,53 @@
 import re
 
 
-def has_special_char(line, index_start, index_end):
-    index_start = max(0, index_start)
-    index_end = min(len(line), index_end)
-    return bool(re.search(r'[^0-9.]', line[index_start:index_end]))
+def find_overlapping(line, start_index, end_index, regex):
+    overlapping = []
+
+    for match in re.finditer(regex, line):
+        digit_start, digit_end = match.span()
+
+        if digit_start < end_index and digit_end > start_index:
+            overlapping.append(match.group())
+
+    return overlapping
 
 
-def is_engine_part(lines, line_idx, part_start_idx, part_end_idx):
+def check_engine_part(lines, line_idx, start_index, end_index):
+    regex = r'[^0-9.]'
     # Check same line
-    if has_special_char(lines[line_idx], part_start_idx-1, part_end_idx+1):
+    if find_overlapping(lines[line_idx], start_index-1, end_index+1, regex):
         return True
 
     # Check previous line
-    if line_idx >= 1 and has_special_char(lines[line_idx-1], part_start_idx-1, part_end_idx+1):
+    if line_idx >= 1 and find_overlapping(lines[line_idx-1], start_index-1, end_index+1, regex):
         return True
 
     # Check next line
-    if line_idx < len(lines) - 1 and has_special_char(lines[line_idx+1], part_start_idx-1, part_end_idx+1):
+    if line_idx < len(lines) - 1 and find_overlapping(lines[line_idx+1], start_index-1, end_index+1, regex):
         return True
 
     return False
 
 
-def find_overlapping(line, start_index, end_index, regex=r'\d+'):
-    overlapping_digits = []
-
-    for match in re.finditer(regex, line):
-        digit_start, digit_end = match.span()
-
-        if digit_start <= end_index and digit_end > start_index:
-            overlapping_digits.append(match.group())
-
-    return overlapping_digits
-
-
 def check_gear(lines, line_idx, idx):
+    regex = r'\d+'
     adjacent_digits = []
 
     # Check same line
-    digits_same_line = find_overlapping(lines[line_idx], idx-1, idx+1)
+    digits_same_line = find_overlapping(lines[line_idx], idx-1, idx+2, regex)
     if digits_same_line:
         adjacent_digits.extend(digits_same_line)
 
     # Check previous line
     if line_idx >= 1:
-        digits_previous_line = find_overlapping(lines[line_idx-1], idx-1, idx+1)
+        digits_previous_line = find_overlapping(lines[line_idx-1], idx-1, idx+2, regex)
         if digits_previous_line:
             adjacent_digits.extend(digits_previous_line)
 
     # Check next line
     if line_idx < len(lines):
-        digits_next_line = find_overlapping(lines[line_idx+1], idx-1, idx+1)
+        digits_next_line = find_overlapping(lines[line_idx+1], idx-1, idx+2, regex)
         if digits_next_line:
             adjacent_digits.extend(digits_next_line)
 
@@ -66,7 +62,7 @@ with open('day3/input.txt', 'r') as file:
 
 engine_parts = [
     [int(match.group()) for match in re.finditer(r'\d+', line)
-        if is_engine_part(lines, i, match.start(), match.end())]
+        if check_engine_part(lines, i, match.start(), match.end())]
     for i, line in enumerate(lines)
 ]
 
@@ -79,6 +75,6 @@ for i, line in enumerate(lines):
         is_gear, digit1, digit2 = check_gear(lines, i, match.start())
         if is_gear:
             gear_digits.append((digit1, digit2))
-     
-total = sum((digits[0]*digits[1] for digits in gear_digits))
+
+total = sum(digit1 * digit2 for digit1, digit2 in gear_digits)
 print(f"Part 2 total: {total}")
