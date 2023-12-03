298ea2b23449ddc8183edd356f8b69da26584801
Marc Gomez <marcgomez@microsoft.com>
2023-12-03 14:39:49 +0100

    Day 3 Part 2

--- day3/main.py
+++ day3/main.py
@@ -23,6 +23,44 @@ def is_engine_part(lines, line_idx, part_start_idx, part_end_idx):
     return False
 
 
+def find_overlapping(line, start_index, end_index, regex=r'\d+'):
+    overlapping_digits = []
+
+    for match in re.finditer(regex, line):
+        digit_start, digit_end = match.span()
+
+        if digit_start <= end_index and digit_end > start_index:
+            overlapping_digits.append(match.group())
+
+    return overlapping_digits
+
+
+def check_gear(lines, line_idx, idx):
+    adjacent_digits = []
+
+    # Check same line
+    digits_same_line = find_overlapping(lines[line_idx], idx-1, idx+1)
+    if digits_same_line:
+        adjacent_digits.extend(digits_same_line)
+
+    # Check previous line
+    if line_idx >= 1:
+        digits_previous_line = find_overlapping(lines[line_idx-1], idx-1, idx+1)
+        if digits_previous_line:
+            adjacent_digits.extend(digits_previous_line)
+
+    # Check next line
+    if line_idx < len(lines):
+        digits_next_line = find_overlapping(lines[line_idx+1], idx-1, idx+1)
+        if digits_next_line:
+            adjacent_digits.extend(digits_next_line)
+
+    if len(adjacent_digits) == 2:
+        return True, int(adjacent_digits[0]), int(adjacent_digits[1])
+
+    return False, None, None
+
+
 with open('day3/input.txt', 'r') as file:
     lines = file.read().splitlines()
 
@@ -32,7 +70,15 @@ engine_parts = [
     for i, line in enumerate(lines)
 ]
 
-print(engine_parts)
-
 total = sum(int(part) for line_parts in engine_parts for part in line_parts)
-print(f"total: {total}")
+print(f"Part 1 total: {total}")
+
+gear_digits = []
+for i, line in enumerate(lines):
+    for match in re.finditer(r'\*', line):
+        is_gear, digit1, digit2 = check_gear(lines, i, match.start())
+        if is_gear:
+            gear_digits.append((digit1, digit2))
+     
+total = sum((digits[0]*digits[1] for digits in gear_digits))
+print(f"Part 2 total: {total}")
